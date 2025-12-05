90703c0982167a8d3db7d0bc1055437c93aff7c7
Luccas Garcia <luccasgmelo.1@gmail.com>
2025-12-04 22:20:10 -0300

    modifição do jogador, e reestruturação do código

--- projeto_final_master/src/main.cpp
+++ projeto_final_master/src/main.cpp
@@ -1,3 +1,5 @@
+// CÓDIGO DO ESP MAIN
+
 #include <Arduino.h>
 #include "internet.h"
 #include <WiFi.h>
@@ -5,131 +7,191 @@
 #include <PubSubClient.h>
 #include <LiquidCrystal_I2C.h>
 
+// variaveis para o mqtt
 const char *mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
 const char *mqtt_client_id = "senai134_esp_main_match_game";
 const char *mqtt_topic_sub = "main_match_game_sub";
 const char *mqtt_topic_pub = "main_match_game_pub";
-String esp1, esp2, esp3;
-String mensagem1, mensagem2, mensagem3;
-bool iniciar1, iniciar2, iniciar3;
-unsigned long tempo1 = 999999999;
-unsigned long tempo2 = 999999999;
-unsigned long tempo3 = 999999999;
-float resposta1, resposta2, resposta3;
 
-String cores[] = {"vermelho", "verde", "azul"};
-// 1 = vermelho
-// 2 = verde
-// 3 = azul
-
-float resposta = 0;
-int perguntaAtual = 0;
-
-bool ninguem = true;
-bool gerados = false;
-bool respondida = true;
-float x, y;
-
+// objetos das bibliotecas
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-int jogadores = 0;
-const int maxPerguntas = 10;
-String pontos[maxPerguntas] = {};
-
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-//PROTOTIPO DAS FUNÇÕES
+// estrutura do objeto jogador
+struct Jogador {
+
+  String esp; // nome do esp que está enviando as mensagens
+  String mensagem; // mensagem enviada
+  bool conectado; // conexao do esp sub com o esp main
+  bool iniciar; // se o esp sub esta pronto
+  float resposta; // resposta que ele envia durante os jogos
+  int pontos; // guarda quantos pontos o jogador fez
+  String cor; // guarda qual a cor do jogador
+
+};
+
+// quantidade de jogadores, jogadores conectados e jogadores prontos
+const int qntdJogadores = 3;
+int jogadoresConectados = 0;
+int jogadoresProntos = 0;
+
+// variavel para dizer que ninguem se conectou
+bool ninguem = 1;
+
+// array para criar e guardar os jogadores
+Jogador jogadores[qntdJogadores];
+
+float respostaVerdadeira = 0; // resposta da expressão numerica gerada pelo main
+float x, y; // numeros aleatorios gerados a cada pergunta
+const int maxPerguntas = 10; // serao feitas 10 perguntas até acabar o jogo
+int perguntasFeitas = 0; // guarda quantas perguntas ja foram
+int indiceResposta = 0; // variavel para guardar onde a resposta deve ser escrita no lcd
+
+// variaveis para garantir que os numeros gerados serão gerados apenas uma vez, até serem respondidos.
+bool gerado = false; 
+bool respondido = true;
+
+// variaveis para mostrar a resposta
+unsigned long tempoResposta = 0;
+bool mostraResposta = false;
+bool acertou = false;
+
+// variaveis para mostrar o vencedor
+unsigned long tempoVencedor = 0;
+bool vencedor = false;
+
+// variavel para iniciar a contagem de jogadores
+bool iniciar = true;
+
+// prototipo das funções
 void conectaMqtt();
 void retornoMqtt(char *, byte *, unsigned int);
-float gerarResposta(float n, float m);
+void gerarResposta(float n, float m);
+void mostrarVencedor();
+void telaInicial();
 
-void setup()
-{
+void setup() {
+
+  // gerando a pseudoaleatoriedade
+  randomSeed(analogRead(0));
+
+  // colocando os nomes de cada esp em seu respectivo jogador
+  jogadores[0].esp = "esp1";
+  jogadores[1].esp = "esp2";
+  jogadores[2].esp = "esp3";
+
+  // colocando a cor de cada jogador
+  jogadores[0].cor = "AZUL";
+  jogadores[1].cor = "VERMELHO";
+  jogadores[2].cor = "VERDE";
+
+  // configurações da serial
   Serial.begin(115200);
-  Serial.setTimeout(10000);
 
+  // inicio do lcd
   lcd.init();
   lcd.backlight();
-  lcd.print("INICIAR?");
-  lcd.setCursor(0, 1);
-  lcd.print("Jogadores: 0");
+  telaInicial();
 
+  // funcao para conectar o wifi
   conectaWiFi();
 
+  // funcao para conectar o mqtt
   client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(retornoMqtt);
+
+  // funcao para ler as mensagens recebidas
+  client.setCallback(retornoMqtt); 
 }
 
-void loop()
-{
+void loop() {
+  
+  // funcao para checar o wifi
   checkWiFi();
 
-  client.loop(); // Atualiza o cliente
+  // atualiza o cliente
+  client.loop();
 
-  if (!client.connected()) // Verifica se não esta conectado
-  {
-    conectaMqtt(); // Executa a função de conectar
+  // garante a conexao continua com o mqtt
+  if (!client.connected()) conectaMqtt();
+
+  if (iniciar) {
+    jogadoresConectados = jogadores[0].conectado + jogadores[1].conectado + jogadores[2].conectado;
+    jogadoresProntos = jogadores[0].iniciar + jogadores[1].iniciar + jogadores[2].iniciar; 
   }
 
-  if (!(jogadores == iniciar1 + iniciar2 + iniciar3) || ninguem) {
+  if ((jogadoresConectados != jogadoresProntos) || ninguem) {
     lcd.setCursor(11, 1);
-    lcd.print(jogadores);
+    lcd.print(jogadoresConectados);
     return;
   }
-
-  if (!gerados) {
-    x = random(0, 11);
-    y = random(0, 11);
-    resposta = gerarResposta(x, y);
-    gerados = true;
-    respondida = false;
+  else {
+    iniciar = false;
+    gerado = false;
   }
 
-  if (!respondida) {
+  if (!gerado) {
+    x = random(0, 11);
+    y = random(0, 11);
+    gerarResposta(x, y);
+    gerado = true;
+    respondido = false;
+  }
 
-    float respostas[] = {resposta1, resposta2, resposta3};
-    unsigned long tempos[] = {tempo1, tempo2, tempo3};
+  if (!respondido) {
 
-    for (int i = 0; i < 3; i++) {
-      if (respostas[i] == resposta && tempos[i] == min(tempo1, min(tempo2, tempo3))) {
-        pontos[perguntaAtual] = cores[i];
-        respondida = true;
-        gerados = false;
+    for (int i = 0; i < qntdJogadores; i++) {
+      if (respostaVerdadeira == jogadores[i].resposta) {
+        jogadores[i].pontos++;
+        perguntasFeitas++;
+        acertou = true;
+        break; // para sair assim que alguem acertar a respota
       }
     }
 
-    resposta1 = 0;
-    resposta2 = 0;
-    resposta3 = 0;
+    // zerando todas as respostas, para evitar pontos infinitos
+    jogadores[0].resposta = 0;
+    jogadores[1].resposta = 0;
+    jogadores[2].resposta = 0;
+
+    // mostra por 3 segundos qual era a resposta da questão
+    if (millis() - tempoResposta >= 3000 && acertou) {
+      mostraResposta = !mostraResposta;
+      tempoResposta = millis();
+    }
+
+    if (mostraResposta && acertou) {
+      lcd.setCursor(0, indiceResposta);
+      lcd.print("      ");
+      lcd.setCursor(0, indiceResposta);
+      lcd.print(respostaVerdadeira);
+    }
+    else if (!mostraResposta && acertou) {
+      lcd.clear();
+      respondido = true;
+      gerado = false;
+      acertou = false;
+    }
   }
 
-  // ---------------- LEITURA DA SERIAL ----------------
-  if (Serial.available() > 0)
-  {
-    String textoDigitado = Serial.readStringUntil('\n');
-    textoDigitado.trim();
-
-    // ---------------- NAO ENVIA TEXTO VAZIO ----------------
-    if (textoDigitado.length() == 0)
-    {
-      Serial.println("Mensagem vazia");
-      return;
+  if (perguntasFeitas == maxPerguntas) {
+    mostrarVencedor();
+    if (millis() - tempoVencedor >= 10000) {
+      tempoVencedor = millis();
+      vencedor = !vencedor;
     }
-    // ---------------- ESTRUTURA O JSON ----------------
-    JsonDocument doc;
-    doc["disp"] = "Esp32MasterMatchGame";
-    doc["msg"] = textoDigitado;
-    doc["time"] = millis();
 
-    String stringJson;
-    serializeJson(doc, stringJson);
-
-    // ---------------- PUBLICA NO MQTT ----------------
-    client.publish(mqtt_topic_pub, stringJson.c_str());
-    Serial.print("Enviado: ");
-    Serial.println(stringJson);
+    if (!vencedor) {
+      gerado = true;
+      respondido = true;
+      iniciar = true;
+      jogadores[0].iniciar = false;
+      jogadores[1].iniciar = false;
+      jogadores[2].iniciar = false;
+      telaInicial();
+      perguntasFeitas = 0;
+    }
   }
 }
 
@@ -160,7 +222,6 @@ void retornoMqtt(char *topic, byte *payload, unsigned int length)
   Serial.print(topic);
   Serial.print(": ");
 
-  // ---------------- MONTA A STRING COM A MENSAGEM RECEBIDA ----------------
   String mensagemRecebida = "";
   for (int i = 0; i < length; i++)
   {
@@ -170,7 +231,6 @@ void retornoMqtt(char *topic, byte *payload, unsigned int length)
   Serial.print("JSON recebido: ");
   Serial.println(mensagemRecebida);
 
-  // ---------------- INTERPRETA JSON RECEBIDO ----------------
   JsonDocument doc;
   DeserializationError erro = deserializeJson(doc, mensagemRecebida);
 
@@ -181,63 +241,29 @@ void retornoMqtt(char *topic, byte *payload, unsigned int length)
     return;
   }
 
-  // ---------------- ACESSA OS CAMPOS JSON ----------------
-  const char *disp = doc["disp"];
+  // save dos valores recebidos
+  const char *esp = doc["esp"];
   const char *msg = doc["msg"];
-  const char* ini = doc["iniciar"];
-  const char* time = doc["tempo"];
-  const char* resp = doc["resposta"];
-  const char* primeira = doc["primeira"];
+  const char *conec = doc["conectado"];
+  const char *ini = doc["iniciar"];
+  const char *resp = doc["resposta"];
 
-  bool b = bool(ini);
-  unsigned long t = long(time);
-  float r = String(resp).toFloat();
-  
-  if (strcmp(disp, "esp1") == 0) {
-    esp1 = disp;
-    mensagem1 = msg;
-    iniciar1 = b;
-    tempo1 = t;
-    resposta1 = r;
-    if (String(primeira).equals("1")) jogadores++; 
-    ninguem = false;
+  // tratamento e atribuição dos valores recebidos
+  for (int i = 0; i < qntdJogadores; i++) {
+    if (jogadores[i].esp.equalsIgnoreCase(esp)) {
+      jogadores[i].mensagem = msg;
+      jogadores[i].resposta = atof(resp);
+      if (strcmp(conec, "1") == 0) {
+        jogadores[i].conectado = true;
+        ninguem = false;
+      }
+      if (strcmp(ini, "1") == 0) jogadores[i].iniciar = true;
+    }
   }
-
-  else if (strcmp(disp, "esp2") == 0) {
-    esp2 = disp;
-    mensagem2 = msg;
-    iniciar2 = b;
-    tempo2 = t;
-    resposta2 = r;
-    if (String(primeira).equals("1")) jogadores++; 
-    ninguem = false;
-  }
-
-  else if (strcmp(disp, "esp3") == 0) {
-    esp3 = disp;
-    mensagem3 = msg;
-    iniciar3 = b;
-    tempo3 = t;
-    resposta3 = r;
-    if (String(primeira).equals("1")) jogadores++; 
-    ninguem = false;
-  }
-
-  Serial.println("Dados recebidos");
-  Serial.print("Dispositivo: ");
-  Serial.println(disp);
-  Serial.print("Mensagem: ");
-  Serial.println(msg);
-  Serial.print("tempo: ");
-  Serial.println(time);
-  Serial.print("Iniciar:");
-  Serial.println(ini);
-  Serial.print("Resposta:");
-  Serial.println(resp);
-  Serial.println("-------------------------");
 }
 
-float gerarResposta(float n, float m) {
+// funcao que recebe numeros aletorios, gera uma expressao numerica e devolve a resposta
+void gerarResposta(float n, float m) {
 
   int alternativa = random(0, 4);
   float r = 0;
@@ -245,7 +271,7 @@ float gerarResposta(float n, float m) {
 
   lcd.clear();
 
-  if (m == 0 && alternativa == 3) alternativa--;
+  if (m == 0 && alternativa == 3) alternativa = random(0, 3);
 
   else {
     if (alternativa == 0) r = n + m;
@@ -254,11 +280,42 @@ float gerarResposta(float n, float m) {
     else if (alternativa == 3) r = n / m;
   }
 
-  lcd.printf("%f %s %f = ??", n, operacoes[alternativa], m);
+  String expressao = String(n) + " " + operacoes[alternativa] + " " + String(m) + " = ??";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(expressao);
 
-  return r;
-
+  respostaVerdadeira = r;
+  indiceResposta = expressao.indexOf("?");
 }
 
-void imprimirResposta
-() {}
+void mostrarVencedor() {
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+
+  int pMax = 0;
+  int indice = 0;
+
+  for (int j = 0; j < qntdJogadores; j++) {
+    if (jogadores[j].pontos >= pMax) pMax = jogadores[j].pontos;
+  }
+
+  for (int i = 0; i < qntdJogadores; i++) {
+    if (jogadores[i].pontos == pMax) indice = i;
+  }
+
+  lcd.print(jogadores[indice].cor);
+  lcd.print(" VENCEU!");
+  lcd.setCursor(0, 1);
+  lcd.print("PARABENS!");
+  
+}
+
+void telaInicial() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("INICIAR?");
+  lcd.setCursor(0, 1);
+  lcd.print("Jogadores: 0");
+}
